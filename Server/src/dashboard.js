fedaf83080a8c7f84e9fdbcb7c1cb38ff375bdb1
JasonGH17 <jason.ghantous@gmail.com>
2021-04-23 15:33:40 +0300

    UI update (Added plotting of USDT balance)

---
+++ Server/src/dashboard.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+
+const balanceContext = document.getElementById("balancechart").getContext("2d");
+
+let balanceChart = new Chart(balanceContext, {
+	type: "line",
+	data: {
+		labels: ["1", "2", "3", "4", "5", "6"],
+		datasets: [
+			{
+				label: "Amount of USDT over time",
+				data: [],
+				backgroundColor: ["rgba(0, 99, 132, 1)"],
+				borderColor: ["rgba(0, 99, 132, 1)"],
+				borderWidth: 1
+			}
+		]
+	},
+	options: {
+		scales: {
+			y: {
+				ticks: {
+					callback: function (value, index, values) {
+						return value + " USDT";
+					}
+				}
+			}
+		}
+	}
+});
+
+function formatAMPM(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? "pm" : "am";
+	hours = hours % 12;
+	hours = hours ? hours : 12;
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	var strTime = hours + ":" + minutes + " " + ampm;
+	return strTime;
+}
+
+function getTrades(chart) {
+	const data = fs.readFileSync("../JSON/Trades.json");
+	const obj = JSON.parse(data)[Object.keys(JSON.parse(data))[0]].splice(-6);
+	chart.data.datasets[0].data = [
+		obj[0].price_usd * obj[0].amount,
+		obj[1].price_usd * obj[1].amount,
+		obj[2].price_usd * obj[2].amount,
+		obj[3].price_usd * obj[3].amount,
+		obj[4].price_usd * obj[4].amount,
+		obj[5].price_usd * obj[5].amount
+	];
+	chart.data.labels = [
+		formatAMPM(new Date(obj[0].time_stamp * 1000)),
+		formatAMPM(new Date(obj[1].time_stamp * 1000)),
+		formatAMPM(new Date(obj[2].time_stamp * 1000)),
+		formatAMPM(new Date(obj[3].time_stamp * 1000)),
+		formatAMPM(new Date(obj[4].time_stamp * 1000)),
+		formatAMPM(new Date(obj[5].time_stamp * 1000))
+	];
+	chart.update();
+}
+
+getTrades(balanceChart);
--- Server/src/main.js
+++ Server/src/main.js
@@ -4,29 +4,11 @@ const togglebtn = document.getElementById("bot-toggle");
 const pairinput = document.getElementById("pair-input");
 const pairsubmit = document.getElementById("submit-pair");
 
-function formatAMPM(date) {
-	var hours = date.getHours();
-	var minutes = date.getMinutes();
-	var ampm = hours >= 12 ? "pm" : "am";
-	hours = hours % 12;
-	hours = hours ? hours : 12;
-	minutes = minutes < 10 ? "0" + minutes : minutes;
-	var strTime = hours + ":" + minutes + " " + ampm;
-	return strTime;
-}
-
-function getTrades() {
-    fs.readFile("../JSON/Trades.json", (err, data) => {
-        if(err) throw err
-        const obj = JSON.parse(data)
-        console.log(obj)
-    })
-}
-
 togglebtn.addEventListener("click", () => {
+    let jsonpair = JSON.parse(fs.readFileSync("../state.json", "utf8")).pair
 	let data = {
 		state: togglebtn.checked,
-		pair: pairinput.value == "" ? ["BTCUSDT"] : [pairinput.value]
+		pair: jsonpair
 	};
 	let jstring = JSON.stringify(data, null, 4);
 	fs.writeFile("../state.json", jstring, (err) => {
@@ -46,5 +28,5 @@ pairsubmit.addEventListener("click", () => {
 	});
 });
 
-console.log(formatAMPM(new Date(1618936740000*1000)));
+formatAMPM(new Date(1618936740000*1000))
 getTrades()
