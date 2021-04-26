00e497439ba8c398d08dc6a9c9a4e3d4d0f0f702
JasonGH17 <jason.ghantous@gmail.com>
2021-04-26 16:04:31 +0300

    Added selected pair price chart in dashboard

--- Server/src/dashboard.js
+++ Server/src/dashboard.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 
 const balanceContext = document.getElementById("balancechart").getContext("2d");
+const priceContext = document.getElementById("pricechart").getContext("2d");
+
+let coin = JSON.parse(fs.readFileSync("../state.json", "utf-8")).pair[0]
 
 let balanceChart = new Chart(balanceContext, {
 	type: "line",
@@ -9,6 +12,33 @@ let balanceChart = new Chart(balanceContext, {
 		datasets: [
 			{
 				label: "Amount of USDT over time",
+				data: [],
+				backgroundColor: ["rgba(0, 99, 132, 1)"],
+				borderColor: ["rgba(0, 99, 132, 1)"],
+				borderWidth: 1
+			}
+		]
+	},
+	options: {
+		scales: {
+			y: {
+				ticks: {
+					callback: function (value, index, values) {
+						return value + " USDT";
+					}
+				}
+			}
+		}
+	}
+});
+
+let priceChart = new Chart(priceContext, {
+	type: "line",
+	data: {
+		labels: ["1", "2", "3", "4", "5", "6"],
+		datasets: [
+			{
+				label: `Price of ${coin} over time`,
 				data: [],
 				backgroundColor: ["rgba(0, 99, 132, 1)"],
 				borderColor: ["rgba(0, 99, 132, 1)"],
@@ -40,9 +70,12 @@ function formatAMPM(date) {
 	return strTime;
 }
 
-function getTrades(chart) {
+function getTrades(chart, pChart) {
 	const data = fs.readFileSync("../JSON/Trades.json");
+    const data2 = fs.readFileSync("../JSON/Data.json")
 	const obj = JSON.parse(data)[Object.keys(JSON.parse(data))[0]].splice(-6);
+    const obj2 = JSON.parse(data2)[Object.keys(JSON.parse(data2))[0]].prices.splice(-9)
+    
 	chart.data.datasets[0].data = [
 		obj[0].price_usd * obj[0].amount,
 		obj[1].price_usd * obj[1].amount,
@@ -59,11 +92,36 @@ function getTrades(chart) {
 		formatAMPM(new Date(obj[4].time_stamp * 1000)),
 		formatAMPM(new Date(obj[5].time_stamp * 1000))
 	];
+
+	pChart.data.datasets[0].data = [
+		obj2[0][4],
+		obj2[1][4],
+		obj2[2][4],
+		obj2[3][4],
+		obj2[4][4],
+		obj2[5][4],
+		obj2[6][4],
+		obj2[7][4],
+		obj2[8][4]
+	];
+	pChart.data.labels = [
+		formatAMPM(new Date(obj2[0][0]* 1000)),
+		formatAMPM(new Date(obj2[1][0]* 1000)),
+		formatAMPM(new Date(obj2[2][0]* 1000)),
+		formatAMPM(new Date(obj2[3][0]* 1000)),
+		formatAMPM(new Date(obj2[4][0]* 1000)),
+		formatAMPM(new Date(obj2[5][0]* 1000)),
+		formatAMPM(new Date(obj2[6][0]* 1000)),
+		formatAMPM(new Date(obj2[7][0]* 1000)),
+		formatAMPM(new Date(obj2[8][0]* 1000))
+	];
+
 	chart.update();
+	pChart.update();
 }
 
-getTrades(balanceChart);
+getTrades(balanceChart, priceChart);
 
 setInterval(() => {
-	getTrades(balanceChart);
+	getTrades(balanceChart, priceChart);
 }, 1000);
